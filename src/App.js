5844aec516f140e7d612f241d544adb2245b3060
henok26 <henokaniguse1931@gmail.com>
2023-12-19 11:12:17 +0300

    added modal and styled form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 import {useState} from 'react'
 import Employee from './components/Employee'
 import './index.css'
+import {v4 as uuidv4} from 'uuid'
 export default function App(){
  
   let role2="manager";
 const [role,setRole]=useState('dev');
+ const [employees,setEmployees]=useState(
+  [
+    {name:"henok",role:"developer",img:"https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {name:"henok",role:"developer",img:"https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {name:"henok",role:"developer",img:"https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {name:"henok",role:"developer",img:"https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {name:"henok",role:"developer",img:"https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {name:"henok",role:"developer",img:"https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {name:"henok",role:"developer",img:"https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {name:"henok",role:"developer",img:"https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+  ]
+ )
   return(   
 
 
@@ -17,10 +30,21 @@ const [role,setRole]=useState('dev');
     console.log(e.target.value)
     setRole(e.target.value)}}/>
 
+<div className='flex flex-wrap justify-center'>
+ 
+{employees.map((employee)=>{
+  console.log(employee);
+  return(
+  <Employee
+  key={uuidv4()}
+   name={employee.name} 
+   role={employee.role} 
+   img={employee.img}/>)
 
-  <Employee name="henok" role="intern"  />
-  <Employee name="Abby" role={role}/>
-  <Employee name="John" role={role2}/>
+})}
+
+
+  </div>
   </div>
   )
  
@@ -28,33 +52,6 @@ const [role,setRole]=useState('dev');
   
 }
 
-// import React from 'react';
-// import Employee from './components/Employee'
-// import { useState } from 'react';
-// export default function App() {
-
-  
-
-//   const showEmployees=false;
-//   const[role,setRole]=useState('dev');
-
-//   return (
-  
-//     <div>
-// <input type='text' onChange={(e)=>{console.log(e.target.value)
-// // role=e.target.value
-// setRole(e.target.value)
-// }}/>
-      
-//       <Employee name="henok" role="intern" age="25"/>
-//       <Employee name="dani" role={role}/>
-//       <Employee name="beti"/>
-      
-      
-      
-//     </div>
-//   );
-// }
 
 
 
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,13 +1,26 @@
+// import "../index.css";
+
+import EditEmployee from "./EditEmployee";
+
 function Employee(props) {
-    return (
-      <div className="bg-red">
-        <h1 className="bg-red">{props.name}</h1 >
-       {props.role?props.role:<p>No Role</p>} 
-        {/* <h1>{props.name}</h1>
-       { props.role?props.role: <p>No Role</p>} */}
+  return (
+    <div className="px-8 pt-16 max-w-sm  bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 m-2">
+    <img className="object-cover h-[100px] w-[100px] block mx-auto  rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt="Woman's Face" />
+    <div className="text-center space-y-2 sm:text-left">
+      <div className="space-y-0.5">
+        <p className="text-lg text-black font-semibold">
+          {props.name}
+        </p>
+        <p className="text-slate-500 font-medium">
+          {props.role}
+        </p>
+
+        <EditEmployee/>
       </div>
-    );
-  }
-  
-  export default Employee;
-  
+     
+    </div>
+  </div>
+  );
+}
+
+export default Employee;
