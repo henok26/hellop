b4416c6a80274582c34d74842d940cc50d5ba941
henok26 <henokaniguse1931@gmail.com>
2024-01-02 17:03:26 +0300

    fetch

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -34,7 +34,7 @@ console.log(props.id,name,role)
   <div className="md:flex md:items-center mb-6">
     <div className="md:w-1/3">
       <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name">
-        Fuly Names
+        Full Name
       </label>
     </div>
     <div className="md:w-2/3">
@@ -47,7 +47,7 @@ console.log(props.id,name,role)
   <div className="md:flex md:items-center mb-6">
     <div className="md:w-1/3">
       <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="role">
-    Rolese
+    Role
       </label>
     </div>
     <div className="md:w-2/3">
